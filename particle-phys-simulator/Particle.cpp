83c4be0d72e7a955a00003396aa181c96e9f01b4
ZayaanRahman <126630377+ZayaanRahman@users.noreply.github.com>
2023-06-18 02:25:11 -0500

    Add files via upload

---
+++ particle-phys-simulator/Particle.cpp
@@ -0,0 +1,9 @@
+// created by Zayaan Rahman
+// 6/7/2023
+//
+// represents individual particle in simulation
+
+#include "Particle.h"
+
+// default constructor
+Particle::Particle() : id(0), radius(0), xpos(0), ypos(0), xvel(0), yvel(0), xacc(0), yacc(0) {}
---
+++ particle-phys-simulator/main.cpp
@@ -0,0 +1,22 @@
+// created by Zayaan Rahman
+// 6/7/2023
+//
+// driver for simulator
+
+#include <iostream>
+
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+
+int main()
+{
+
+    // configure engine settings from file and take in particle arrangements
+
+
+    // run visualizer
+
+
+    return 0;
+}
