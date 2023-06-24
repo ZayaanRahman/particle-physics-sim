0cd60ab24a480a3faf8570601e8b916f539d6450
ZayaanRahman <m.zayaan.r@gmail.com>
2023-06-24 18:22:54 -0500

    Created resolver decalration (also pushes on github)

--- CollisionResolver.h
+++ CollisionResolver.h
@@ -6,8 +6,34 @@
 #ifndef PARTICLE_PHYS_SIMULATOR_COLLISIONRESOLVER_H
 #define PARTICLE_PHYS_SIMULATOR_COLLISIONRESOLVER_H
 
+#include "Particle.h"
+
+#include <vector>
+#include <utility>
 
 class CollisionResolver {
+
+  private:
+
+    // space dimensions
+    size_t width;
+    size_t height;
+
+    // Reference to vector particles in engine
+    std::vector<Particle>& particlesRef;
+
+  public:
+
+    // default constructor and constructor using engine settings
+    CollisionResolver();
+    CollisionResolver(std::vector<Particle> particles, size_t width, size_t height);
+
+    // resolve particle-particle collisions
+    void resolveParticleCol(std::vector<std::pair<Particle, Particle>> particleCollisions);
+
+    // resolve particle-boundary collisions
+    void resolveBoundaryCol(std::vector<std::pair<Particle, size_t>> boundaryCollisions);
+
 
 };
 
